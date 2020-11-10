7271be6ea19f9879786bd5340291dc360cabdf0b
lesley okoduwa <45374070+oselesley@users.noreply.github.com>
2020-11-10 21:41:56 +0100

    create userDetailServiceRepositories

--- src/main/java/com/fcmb/usersecurity/repositories/UserSecurityDetailRepository.java
+++ src/main/java/com/fcmb/usersecurity/repositories/UserSecurityDetailRepository.java
@@ -1,4 +1,16 @@
 package com.fcmb.usersecurity.repositories;
 
-public class UserSecurityDetailRepository {
+import com.fcmb.usersecurity.models.User;
+import com.fcmb.usersecurity.models.UserSecurityDetail;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface UserSecurityDetailRepository extends JpaRepository<UserSecurityDetail, Long> {
+    List<UserSecurityDetail> findUserSecurityDetailByUser (User user);
+
+    Optional<UserSecurityDetail> findUserSecurityDetailByDeviceId (String deviceId);
 }
