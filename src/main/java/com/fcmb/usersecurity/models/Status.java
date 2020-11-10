16eccf00dae5cccefa0fe769c768431242cf56d9
lesley okoduwa <45374070+oselesley@users.noreply.github.com>
2020-11-10 21:40:57 +0100

    create user and status models

--- src/main/java/com/fcmb/usersecurity/models/Status.java
+++ src/main/java/com/fcmb/usersecurity/models/Status.java
@@ -1,4 +1,41 @@
 package com.fcmb.usersecurity.models;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
 public class Status {
+    private User user;
+    Boolean twoFactorEnforced;
+    Boolean limitEnforced;
+    BigDecimal limit;
+    BigDecimal totalTransactionAmount;
+
+    public Status withUser(User user) {
+        this.user = user;
+        return this;
+    }
+
+    public Status withtwoFactorEnforced(boolean twoFactorEnforced) {
+        this.twoFactorEnforced = twoFactorEnforced;
+        return this;
+    }
+
+    public Status withLimitEnforced(boolean limitEnforced) {
+        this.limitEnforced = limitEnforced;
+        return this;
+    }
+
+    public Status withLimit(BigDecimal limit) {
+        this.limit = limit;
+        return this;
+    }
+
+    public Status withTotalTransactionAmount(BigDecimal totalTransactionAmount) {
+        this.totalTransactionAmount = totalTransactionAmount;
+        return this;
+    }
 }
--- src/main/java/com/fcmb/usersecurity/models/User.java
+++ src/main/java/com/fcmb/usersecurity/models/User.java
@@ -1,4 +1,8 @@
 package com.fcmb.usersecurity.models;
+/**
+ * A placeholder model to simulate users in the app
+ */
+
 
 public class User {
 }
